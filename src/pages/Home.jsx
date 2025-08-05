88f5263371b20f1c4f5ce40cccd9a4e353a51713
ivan-pardo <yvanpardoalcocer@gmail.com>
2025-08-05 19:50:43 -0300

    add: contenido estatico en Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,36 @@ import { Layout } from "../components/Layout"
 const Home = () => {
   return (
     <Layout>
-      <h1>Hola desede Home</h1>
+      <section>
+        <h1>Bienvenido a Nuestra Tienda</h1>
+        <p>Descubrí una seleccion exclusiva de productos para vos. Calidad, confianza y atencion personalizada</p>
+      </section>
+      <section>
+        <h2>¿Por que elegirnos?</h2>
+        <ul>
+          <li>
+            <h3>Envios a todo el pais</h3>
+            <p>Recibi tu compra en la puerta de tu casa o donde estes</p>
+          </li>
+          <li>
+            <h3>Pagos seguros</h3>
+            <p>Trabajamos con plataformas que garantizan tu seguridad</p>
+          </li>
+          <li>
+            <h3>Atencion personalizada</h3>
+            <p>Estamos disponibles para ayudarte en todo momento</p>
+          </li>
+        </ul>
+      </section>
+
+      <section>
+        <h2>Nuestros productos</h2>
+        <p>Elegi entre nuestras categorias mas populares</p>
+        {/* {Seccion para carga diferida de productos} */}
+        <div>
+          <p>Cargando producto...</p>
+        </div>
+      </section>
     </Layout>
   )
 }
