d943b9feb576b5922096ad511d02b92117354044
ivan-pardo <yvanpardoalcocer@gmail.com>
2025-08-06 20:40:05 -0300

    feat: formulario de registro con validaciones

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,13 @@
+import { Link } from "react-router-dom"
 import { Layout } from "../components/Layout"
 
 const NotFound = () => {
   return (
     <Layout>
-      <h1>Hola desde NotFound</h1>
+      <h1>Pagina no encontrada</h1>
+      <p>Lo sentimos, la pagina que estas buscando  no existe.</p>
+      <p>Verifica la URL o volvé al inicio</p>
+      <Link to="/">Ir a Inicio</Link>
     </Layout>
   )
 }
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,30 +1,72 @@
+import { useState } from "react";
 import { Layout } from "../components/Layout";
 
 const Register = () => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (!username || !email || !password) {
+      setError("Error al introducir datos");
+      return;
+    }
+    setError(""); 
+
+    const newUser = {
+      user: username,
+      email: email,
+      password: password,
+    };
+    console.log(newUser);
+    setUsername("");
+    setEmail("");
+    setPassword("");
+  };
+
   return (
     <Layout>
       <h1>Hola desde Register</h1>
 
       <section>
         <h2>Hola, Bienvenido</h2>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <label>Username:</label>
-            <input type="text" name="nombre" />
+            <input
+              type="text"
+              name="nombre"
+              onChange={(e) => setUsername(e.target.value)}
+              value={username}
+            />
           </div>
 
           <div>
             <label>Email:</label>
-            <input type="email" name="email" />
+            <input
+              type="email"
+              name="email"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
           </div>
 
           <div>
             <label>Contraseña:</label>
-            <input type="password" name="password" />
+            <input
+              type="password"
+              name="password"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+            />
           </div>
 
           <button>Ingresar</button>
         </form>
+        {error && <p>{error}</p>}
       </section>
     </Layout>
   );
